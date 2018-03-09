254df8dcc6b657232ec732dbf4e4b968671dd956
LuckyCharmStudio <37159062+LuckyCharmStudio@users.noreply.github.com>
2018-03-09 16:13:12 -0500

    New Character and simple Ball script.

---
+++ Ball.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public float moveSpeed;
+    public float maxSpeed;
+    public float jumpHeight;
+    private Rigidbody2D ballRigid;
+
+	// Use this for initialization
+	void Start () {
+        ballRigid = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxis("Horizontal") * moveSpeed;
+
+        ballRigid.AddForce(Vector2.right * h);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            ballRigid.AddForce(Vector2.up * jumpHeight);
+        }
+        
+    }
+}
